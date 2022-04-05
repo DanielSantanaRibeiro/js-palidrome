6606631d180feac6504e5f48f0325135677cf4c0
Daniel Santana Ribeiro <danielsantanaribeiro2015@gmail.com>
2022-04-04 23:48:58 -0300

    Renaming 'palindrome.js' to 'index.js'

--- index.js
+++ index.js
@@ -3,7 +3,5 @@ function isPalindrome(word) {
     return word === reverseWord;
 }
 
-const word = "arara";
-
-console.log(`${word} is a palidrome ? :  ${isPalindrome(word)}`);
+module.exports = isPalindrome;
 
